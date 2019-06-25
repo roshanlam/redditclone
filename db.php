7f6a10b41a0b6844caeaa15072f3c13da9a1129c
Roshan Lamichhane <33913826+roshanlam@users.noreply.github.com>
2019-06-24 21:22:30 -0400

    Update db.php

--- db.php
+++ db.php
@@ -5,3 +5,10 @@ $user = '';
 $pass = '';
 $db = '';
 $mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);
+
+// Check connection
+if ($mysqli->connect_error) {
+    die("Connection failed: " . $mysqli->connect_error);
+} 
+echo "Connected successfully";
+?>
